f9e6fdcecf5cee09096a8587fa15c3fef1b2ad2d
tobymould <toby.mould@icloud.com>
2020-07-16 13:34:50 +0100

    implemented basic search functionality through portfolio project buttons based on prpject title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ import Footer from '../Footer';
 import projects from '../../data/data';
 
 export class Home extends Component {
-  state = { modalState: false, projectState: null, eventSet: null };
+  state = { modalState: false, projectState: null, eventSet: null, searchTerm: null };
   //
 
   modalToggle = event => {
@@ -42,7 +42,7 @@ export class Home extends Component {
 
   modalOpenFunction = projectButtonClicked => {
     const project = projects[projectButtonClicked];
-    console.log(project);
+    // console.log(project);
 
     if (this.state.modalState == true) {
       return <Modal project={this.state.projectState} modalState={this.state.modalState} modalOpenFunction={this.modalOpenFunction} modalToggle={this.modalToggle} />;
@@ -52,13 +52,38 @@ export class Home extends Component {
     // });
   };
 
+  setSearchTerm = event => {
+    this.setState({ searchTerm: event.target.value });
+    console.log(event.target.value);
+  };
+
+  searchProjects = () => {
+    const { searchTerm } = this.state;
+    const term = searchTerm.toLowerCase();
+    console.log(term);
+
+    return projects.filter((project, index) => {
+      const projectName = project.name.toLowerCase();
+      console.log(projectName);
+
+      if (projectName.includes(term)) {
+        return (
+          <button className={styles.item} value={index} key={index} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${project.image}) no-repeat center center/cover` }} onClick={this.modalToggle}>
+            <h4>{project.name}</h4>
+          </button>
+        );
+      }
+    });
+    console.log(this.state.searchterm);
+  };
+
   render() {
     return (
       <>
         <Landing />
         <Navbar />
         <Skills />
-        <Portfolio modalState={this.state.modalState} projectState={this.state.projectState} modalOpenFunction={this.modalOpenFunction} modalToggle={this.modalToggle} setButtonPressed={this.setButtonPressed} setModalProject={this.setModalProject} />
+        <Portfolio modalState={this.state.modalState} projectState={this.state.projectState} modalOpenFunction={this.modalOpenFunction} modalToggle={this.modalToggle} setButtonPressed={this.setButtonPressed} setModalProject={this.setModalProject} searchTerm={this.state.searchTerm} setSearchTerm={this.setSearchTerm} searchProjects={this.searchProjects} />
         <Background />
         <Footer />
       </>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,7 +9,7 @@ import Modal from '../Modal';
 export class Portfolio extends Component {
   //
   addPortfolioButtons = () => {
-    const { modalOpenFunction, modalToggle } = this.props;
+    const { modalToggle } = this.props;
     return projects.map((project, index) => {
       return (
         <button className={styles.item} value={index} key={index} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${project.image}) no-repeat center center/cover` }} onClick={modalToggle}>
@@ -20,17 +20,37 @@ export class Portfolio extends Component {
     });
   };
 
+  searchProjects = () => {
+    const { modalToggle, searchTerm } = this.props;
+    const term = searchTerm.toLowerCase();
+    console.log(term);
+
+    return projects.map((project, index) => {
+      const projectName = project.name.toLowerCase();
+      console.log(projectName);
+
+      if (projectName.includes(term)) {
+        return (
+          <button className={styles.item} value={index} key={index} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${project.image}) no-repeat center center/cover` }} onClick={modalToggle}>
+            <h4>{project.name}</h4>
+          </button>
+        );
+      }
+    });
+    console.log(this.state.searchterm);
+  };
+
   render() {
-    const { modalState, projectState, modalOpenFunction } = this.props;
+    const { modalState, projectState, modalOpenFunction, setSearchTerm, searchTerm } = this.props;
     return (
       <section className={styles.portfolioWrapper}>
         <div className={styles.container}>
           <h2>Portfolio</h2>
           <div>
             <FontAwesomeIcon icon={['fas', 'search']} />
-            <input type="search" placeholder="Search projects..." />
+            <input type="search" placeholder="Search projects..." onChange={setSearchTerm} />
           </div>
-          <section className={styles.items}>{this.addPortfolioButtons()}</section>
+          <section className={styles.items}>{searchTerm ? this.searchProjects() : this.addPortfolioButtons()}</section>
           <div>
             {modalState ? modalOpenFunction() : null}
             {/* <Modal project={projectState} modalState={modalState} modalOpenFunction={modalOpenFunction} /> */}
